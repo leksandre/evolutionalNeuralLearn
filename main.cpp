3f04810dc9a7a3798b0e6c5dab7b8bd6d7204745
leksandre <44774042+leksandre@users.noreply.github.com>
2024-01-11 13:01:34 +0500

    heuroVisualisation2

--- main.cpp
+++ main.cpp
@@ -1191,11 +1191,22 @@ void simple1() {
 
 }
 
+int getVersion() {
+    if (__cplusplus == 202101L) std::cout << "C++23";
+    else if (__cplusplus == 202002L) std::cout << "C++20";
+    else if (__cplusplus == 201703L) std::cout << "C++17";
+    else if (__cplusplus == 201402L) std::cout << "C++14";
+    else if (__cplusplus == 201103L) std::cout << "C++11";
+    else if (__cplusplus == 199711L) std::cout << "C++98";
+    else std::cout << "pre-standard C++." << __cplusplus;
+    std::cout << "\n";
+}
+
 
 int main(int argc, char *argv[])
 {
 
-
+    getVersion();
     bb = new myNeuro();
     
 
